ed4326f1334a8c3b7498f9a4e8593bd4780b786b
YoutubeSoundsystem <52357115+YoutubeSoundsystem@users.noreply.github.com>
2019-06-29 19:28:48 -0400

    Update playerFunctions.js

--- BS2/js/playerFunctions.js
+++ BS2/js/playerFunctions.js
@@ -235,7 +235,7 @@ BSplayer.clearQueue = function(){				//Removes all but the first videoEntry (the
 }
 BSplayer.play = function(){
 	player.playVideo();
-	DOM.Player.style.display = "none";
+	 document.getElementById("player").style.display = "none";
 	if (this.currentVideo != undefined){
 		this.isPlaying = true;
 	}
