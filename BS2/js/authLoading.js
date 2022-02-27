8cfd0ff3559d416dff739de20314c3fe4171627b
YoutubeSoundsystem <52357115+YoutubeSoundsystem@users.noreply.github.com>
2022-02-27 17:32:33 -0500

    Update authLoading.js

--- BS2/js/authLoading.js
+++ BS2/js/authLoading.js
@@ -106,7 +106,7 @@ function updateSigninStatus(isSignedIn) {
 		SignedIn = true;
 		document.getElementsByClassName("menuItemButton")[0].style.display = "none";
 		DOM.Topbar.AccountImage.style.display = "block";	
-		DOM.Topbar.AccountImage.style.backgroundImage = "url("+user.wt.SJ+")";
+		DOM.Topbar.AccountImage.style.backgroundImage = "url("+user.Du.eN+")";
 
 		if (PlayerIn == true){
 			startHashHandling();
